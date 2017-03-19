ccfe630a4c5b726f96fdff660baaa5af03500646
rkrobo <rolakitaphanich@hotmail.com>
2017-03-18 23:44:30 -0700

    Initial Commit

---
+++ FoodETests/FoodETests.swift
@@ -0,0 +1,36 @@
+//
+//  FoodETests.swift
+//  FoodETests
+//
+//  Created by Rola Kitaphanich on 2017-03-18.
+//  Copyright © 2017 Rola Kitaphanich. All rights reserved.
+//
+
+import XCTest
+@testable import FoodE
+
+class FoodETests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
